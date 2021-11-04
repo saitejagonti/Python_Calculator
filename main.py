68dd2bce1d17bcdeb384f28fc7c0ca85d0366db6
saiteja <saitejagonti@gmail.com>
2021-11-04 19:03:35 +0530

    This is my Second commit

--- main.py
+++ main.py
@@ -2,24 +2,25 @@
 
 def addition(a,b):
     result=a+b
-    print(result)
+    print(f'The Addition of {a} and {b} is {result}')
 
 def substraction(a,b):
     result=a-b
-    print(result)
+    print(f'The Substraction of {a} and {b} is {result}')
 
 def multipication(a,b):
     result=a*b
-    print(result)
+    print(f'The Multipication of {a} and {b} is {result}')
 
 def division(a,b):
     result=a/b
-    print(result)
+    print(f'The Division of {a} and {b} is {result}')
 
-
-a=int(input("Enter a number: "))
-b=int(input("Enter a number: "))
-op=(input("Enter a Operator: "))
+n =(input("Please Enter Your Name: "))
+print(f'Hi {n}, Select the Numbers and Operator for Calculation')
+a=int(input("Enter a First number: "))
+b=int(input("Enter a  Second number: "))
+op=(input("Now Select the Operator: "))
 
 if op=="+":
     addition(a,b)
